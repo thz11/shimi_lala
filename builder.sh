ef75bab2ef2d27b70afffa51e2cd5d386f6866d7
thz22 <thjr5965@gmail.com>
2025-02-22 15:58:54 -0300

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -29,7 +29,7 @@ mkdir lineage2
 cd lineage2
 
 # init repo
-repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs --depth=1
+repo init -u https://github.com/thz22/android.git -b 14.0 --git-lfs --depth=1
 
 # clone manifest
 git clone https://github.com/thz22/local_manifest_odessa.git -b main .repo/local_manifests
