59ce08b6652105edad0ce6303ea0c3f83605a150
thz <diasjuremathiago@gmail.com>
2025-04-05 12:25:02 -0300

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -2,7 +2,7 @@
 sudo apt update
 
 # Install dependencies
-sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+sudo apt install bc bison build-essential ccache curl flex libncurses5 g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32readline-dev lib32z1-dev liblz4-tool libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 
 # setup repo
 mkdir ~/bin
@@ -32,7 +32,7 @@ cd arrow
 repo init -u https://github.com/thz11/android_manifest.git -b arrow-13.1 --git-lfs --depth=1
 
 # clone manifest
-git clone https://github.com/thz11/local_manifests_blossomy.git -b main .repo/local_manifests
+git clone https://github.com/thz11/local_manifest_odessa.git -b los13 .repo/local_manifests
 
 # sync
 repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j8
